472511d8cd731a118eb77c0381d082c67e006376
Kshitiz Karki <karki.kshitiz.23@gmail.com>
2023-10-18 11:04:49 +0545

    Add solution to exercise for 4.20

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -8,13 +8,20 @@ const requestLogger = (request, response, next) => {
 	next()
 }
 
+const tokenExtractor = (request, response, next) => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.startsWith('Bearer ')) {
+		request.token = authorization.replace('Bearer ', '')
+	}
+	next()
+}
+
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
 	logger.error(error.message)
-
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' })
 	} else if (error.name === 'ValidationError') {
@@ -32,6 +39,7 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
 	requestLogger,
+	tokenExtractor,
 	unknownEndpoint,
 	errorHandler,
 }
