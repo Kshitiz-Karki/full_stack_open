34c17a3f788bd6739214085af5c3a466b17332c5
Kshitiz Karki <karki.kshitiz.23@gmail.com>
2023-10-27 15:52:32 +0545

    Add solution to exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,12 +2,19 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if (state.filter){
+      return state.anecdotes.filter(x => x.content.includes(state.filter))
+    } else {
+      // console.log('filter not present');
+      return state.anecdotes
+    }
+  })
 
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    console.log('vote', id)
+    // console.log('vote', id)
     dispatch(voteAnecdote(id))
   }
 
